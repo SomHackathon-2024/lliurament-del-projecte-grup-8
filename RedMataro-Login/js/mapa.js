4b4c12d42e51d2de3a9edb9d080d4d5dbb5316d3
VitalityDoS <68471870+VitalityDoS@users.noreply.github.com>
2024-02-18 01:24:54 +0100

    añadir puntos de interes al mapa

--- RedMataro-Login/js/mapa.js
+++ RedMataro-Login/js/mapa.js
@@ -12,14 +12,72 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const restaurantes = [
     { nombre: "Bar Casa Pepe, Mataró", latitud: 41.5405513, longitud: 2.4343848 },
+    { nombre: "Terra Alta Restaurant (En aquest establiment pots bescanviar els teus punts de l'aplicació)", latitud: 41.543196368353705, longitud: 2.435462528844411 },
     { nombre: "La Marineta Restaurant", latitud: 41.5365900, longitud: 2.4412564 },
     { nombre: "Meraki17 Restaurante Mediterráneo Fusión", latitud: 41.5368731, longitud: 2.4409329 },
     { nombre: "Restaurante Can Dimas", latitud: 41.5360067, longitud: 2.4497396 }
     // Agrega más restaurantes según sea necesario
 ];
 
+    // Añadimos las constantes de los perfiles (para filtrar según intereses del usuario)
+    const relax = [
+        
+        { nombre: "La Marineta Restaurant", latitud: 41.5365900, longitud: 2.4412564 },
+        { nombre: "Casa Coll i Regàs", latitud: 41.540837764466666, longitud: 2.441992576699356 },
+        { nombre: "Meraki17 Restaurante Mediterráneo Fusión", latitud: 41.5368731, longitud: 2.4409329 },
+        { nombre: "Restaurante Can Dimas", latitud: 41.5360067, longitud: 2.4497396 }
+    ];
+    
+    const explorador = [
+        { nombre: "Terra Alta Restaurant (En aquest establiment pots bescanviar els teus punts de l'aplicació)", latitud: 41.543196368353705, longitud: 2.435462528844411 },
+        { nombre: "Parc Forestal", latitud: 41.56816918761738, longitud: 2.4272126970114294},
+        { nombre: "Turó de la Creu d'en Serra", latitud: 41.575405555897255, longitud: 2.427424474180397 },
+        { nombre: "Basílica de Santa María", latitud: 41.54131893261159, longitud: 2.44815470040748 },
+        { nombre: "Biblioteca Antoni Comas", latitud: 41.54497642976675, longitud: 2.454816240693977 }
+    ];
+    
+    const familia = [
+        { nombre: "Centre de Mataró", latitud: 41.538234, longitud: 2.444938 },
+        { nombre: "Platja del Varador", latitud: 41.5357833366709, longitud: 2.451897267278119 },
+        { nombre: "Bosc vertical (Parc forestal)", latitud: 41.568023064254845, longitud:  2.437967767506894 },
+        { nombre: "Restaurant Salicornia Guingueta", latitud: 41.53510831054104, longitud: 2.449249950338159 },
+        { nombre: "Mataró Parc (Centre comercial)", latitud: 41.55585550650456, longitud: 2.436958946565559 },
+        { nombre: "Parc central", latitud: 41.54586910044063, longitud: 2.4411031743767 }
+    ];
+    
+    const joven = [
+        { nombre: "Clap (Discoteca)", latitud: 41.53383160223678, longitud: 2.4314363672594297 },
+        { nombre: "Cocoa (Discoteca)", latitud: 41.53278753471258, longitud: 2.4294622615018664 },
+        { nombre: "Miracle (Discoteca)", latitud: 41.53284375416237, longitud: 2.4274237827304788 },
+        { nombre: "Birrateka (Bar)", latitud: 41.53752756668709, longitud: 2.4501880055254524 },
+        { nombre: "GRILL&TACOS", latitud: 41.552796671124845, longitud: 2.451764967896105 },
+        { nombre: "Parc central", latitud: 41.54586910044063, longitud: 2.4411031743767 },
+        { nombre: "Mataró Parc (Centre comercial)", latitud: 41.55585550650456, longitud: 2.436958946565559 },
+        { nombre: "Port de Mataró", latitud: 41.53219165396947, longitud: 2.448312320493979 }
+        // Agrega más restaurantes según sea necesario
+    ];
+
 // Crear y agregar marcadores para cada restaurante
 restaurantes.forEach(restaurante => {
+    L.marker([restaurante.latitud, restaurante.longitud]).addTo(mymap)
+        .bindPopup(restaurante.nombre);
+});
+// Llamamos lo mismo para el resto de constantes
+explorador.forEach(restaurante => {
+    L.marker([restaurante.latitud, restaurante.longitud]).addTo(mymap)
+        .bindPopup(restaurante.nombre);
+});
+
+familia.forEach(restaurante => {
+    L.marker([restaurante.latitud, restaurante.longitud]).addTo(mymap)
+        .bindPopup(restaurante.nombre);
+});
+
+joven.forEach(restaurante => {
+    L.marker([restaurante.latitud, restaurante.longitud]).addTo(mymap)
+        .bindPopup(restaurante.nombre);
+});
+relax.forEach(restaurante => {
     L.marker([restaurante.latitud, restaurante.longitud]).addTo(mymap)
         .bindPopup(restaurante.nombre);
 });
